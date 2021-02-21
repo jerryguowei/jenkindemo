5485cc89b2ab7e305dcb75e0ce15431a345fb47a
jerryguowei <jerryguowei@gmail.com>
2021-02-21 22:05:07 +1100

    jenkin

---
+++ src/main/java/com/duduanan/demo/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.duduanan.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "hello world";
+	}
+}
